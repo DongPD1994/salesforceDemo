c4275010780e87d7d0e06d9c0c2a27d817127411
Đông <dong.pd1994@outlook.com>
2021-04-05 13:53:34 +0700

    Add files via upload

--- force-app/main/default/lwc/addEditPersonalInfo/addEditPersonalInfo.js
+++ force-app/main/default/lwc/addEditPersonalInfo/addEditPersonalInfo.js
@@ -3,27 +3,33 @@ import insertInfo from '@salesforce/apex/AddEditPersonalInfo.insertInfo';
 import { getToastMessage, jsonParse, TYPE_MESS } from 'c/utils';
 
 export default class AddEditPersonalInfo extends LightningElement {
-    @api inputData;
-    @api allField;
+  @api inputData;
+  @api allField;
+  // @api refreshTableList;
 
-    // get getFirstNameValue() {
-    //     return this.inputData.first_name__c;
-    // }
-    get getField(){
-        const tmpField = this.allField;
-        return jsonParse(JSON.stringify(tmpField));
-    }
-    submitInfo(event) {
-        const allValue = this.template.querySelectorAll('lightning-input');
-        const dataInsert = {};
-        if (allValue.length > 0) {
-            allValue.forEach(element => {
-                const key = element.name;
-                dataInsert[key] = element.value;
-            });
-        }
-        const result = insertInfo({insertInfo: dataInsert});
-        this.dispatchEvent(getToastMessage(TYPE_MESS.Success, `Records updared`));
+  get getField() {
+    const tmpField = this.allField;
+    return jsonParse(JSON.stringify(tmpField));
+  }
 
+  submitInfo(event) {
+    const allValue = this.template.querySelectorAll('lightning-input');
+    const dataInsert = {};
+    if (allValue.length > 0) {
+      allValue.forEach(element => {
+        const key = element.name;
+        dataInsert[key] = element.value;
+      });
     }
+    const result = insertInfo({ insertInfo: dataInsert });
+    this.dispatchEvent(getToastMessage(result ? TYPE_MESS.Success : TYPE_MESS.Error, result ? `Records insert success` : `Records insert faild`));
+    if (result) {
+      // this.refreshTableList(true);
+      this.dispatchEvent(new CustomEvent('refresh', { detail: { isRefresh: true } }),
+        {
+          bubbles: true,
+          // composed: true 
+        });
+    }
+  }
 }
